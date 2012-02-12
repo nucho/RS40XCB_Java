55e91240439ed87daef6488ed7bdcb05431373df
nucho <javatea@it.willcomlive.jp>
2012-02-13 02:09:54 +0900

    Release v1.0

--- rs40xcb_Java/src/sample.java
+++ rs40xcb_Java/src/sample.java
@@ -9,32 +9,31 @@ public class sample {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		RS40XCB servo = new RS40XCB("COM17", 115200);
+		RS40XCB servo = new RS40XCB("/dev/ttyUSB0", 115200);
 
-		System.out.println("ID1�̃T�[�{�̃g���N���I��");
+		System.out.println("Torque of the servo of ID1 is set to ON\n");
 		servo.torque(1, true);
 
-		System.out.println("�ō����x��100�x�̈ʒu�։�]");
+		System.out.println("Rotate 100 degrees to the position of maximum speed.");
 		servo.move(1, 1000, 0);
 		try {
-			Thread.sleep(500);//���΂��҂�
+			Thread.sleep(1500);//wait
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("���݊p�x:"+servo.getAngle(1));
+		System.out.println("Angle:"+servo.getAngle(1));
 		
-		System.out.println("1�b������0�x�̈ʒu��");
+		System.out.println("Position of 0 degrees to over 1 second.");
 		servo.move(1, 0, 100);
 		
 		try {
-			Thread.sleep(1000);//���΂��҂�
+			Thread.sleep(1500);//wait
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("���݊p�x:"+servo.getAngle(1));
-		
+		System.out.println("Angle:"+servo.getAngle(1));
 	}
 
 }
